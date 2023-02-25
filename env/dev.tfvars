a4fd891bc7c096e4f872003a7ae34d00cf8814f2
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-25 12:25:03 +0530

    updating terraform-roboshop-mutable

---
+++ env/dev.tfvars
@@ -0,0 +1,3 @@
+VPC_CIDR = "10.0.0.0/16"
+PROJECT = "roboshop"
+ENV = "dev"
--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
-module "tf-module-vpc" {
+module "vpc" {
   source = "github.com/GurukaYadav/tf-module-vpc"
+  VPC_CIDR = var.VPC_CIDR
+  PROJECT = var.PROJECT
+  ENV = var.ENV
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-sfiles"
+    key    = "terraform/mutable/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
